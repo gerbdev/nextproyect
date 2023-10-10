1774ef40a028139f1ccefe083c3981a27920388d
gerbdev <gerdabaralv@gmail.com>
2023-10-10 16:54:59 -0500

    Se agrega el Proyecto

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Users from "@/components/Users";
+
+async function fetchUsers(params) {
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  return data.data;
+}
+async function HomePage() {
+  const users = await fetchUsers();
+
+  return <Users users={users} />;
+}
+
+export default HomePage;
